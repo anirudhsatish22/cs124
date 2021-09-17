e96cbc3a293a93b48095de649ec33048fb783ad3
anirudhsatish22 <asatish@hmc.edu>
2021-09-17 01:03:41 -0700

    added functionality day1

---
+++ script.js
@@ -0,0 +1,28 @@
+let inputText = document.querySelector('#inputToDo');
+let enterButton = document.querySelector('#enter');
+let toDoList = document.querySelector('#list');
+
+
+function enterB() {
+    let txt = inputText.value;
+    let li = document.createElement('li');
+    li.innerHTML= txt;
+    toDoList.insertBefore(li, toDoList.childNodes[0]);
+    inputText.value = '';
+
+
+}
+
+function deleteAll() {
+    // change to delete all checked children
+    while (toDoList.firstChild) {
+        toDoList.removeChild(toDoList.firstChild);
+    }
+
+}
+toDoList.addEventListener('click', e=> {
+    if (e.target.tagName == 'LI'){
+        e.target.classList.toggle('checked');
+    }
+
+})
